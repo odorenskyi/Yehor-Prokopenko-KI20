9dbd3f21d1d5a10b408aff3c6c6d40d168375fab
Tuhgtuhgtuhg <tuhgtuhgtuhg@gmail.com>
2021-04-09 11:12:54 +0300

    Added project PROKOPENKO_task

---
+++ lab08/prj/PROKOPENKO_task/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <wchar.h>
+#include <iomanip>
+#include "modulesprokopenko.h"
+using namespace std;
+
+
+wstring devInfo(){
+    wstring info = L"╔══════════════════════════════════════════════════════╗\n"
+                    "║Виконав студент групи КБ20 Прокопенко Єгор Сергійович©║\n"
+                    "╚══════════════════════════════════════════════════════╝\n";
+    return info;
+}
+
+bool aBCondition( wchar_t a,wchar_t b){
+    bool result = a + 1 == b + 2;
+    return result;
+}
+
+double validator(wstring number){
+    if (number[0] == L'x' || number[0] == 'X')
+        number = L"0" + number;
+    // if the variable is an "inadequate" string of type "0bred56" then the function will return 0
+    return std::stof(number);
+}
+
+double hexS_calculation(wstring x,wstring y, wstring z){
+    return s_calculation(validator(x),validator(y),validator(z));
+}
+
+int main(){
+    consoleToUtf8();
+    wcout << devInfo();
+    wstring x = L"",y = L"",z = L"";
+   wchar_t  a = L' ', b = L' ';
+    wcout <<L"Введіть дійсне число x: ";
+    wcin >> x;
+    wcout <<L"Введіть дійсне число y: ";
+    wcin >> y;
+    wcout <<L"Введіть дійсне число z: ";
+    wcin >> z;
+    wcout <<L"Введіть символ a: ";
+    wcin >> a;
+    wcout <<L"Введіть символ b: ";
+    wcin >> b;
+    wcout << boolalpha << L"Результат логічного виразу: "<<aBCondition(a,b) << endl;
+    wcout << L"Результат роботи модулю s_calculation: "<< hexS_calculation(x,y,z) << endl;
+    system("pause");
+    return 0;
+}
